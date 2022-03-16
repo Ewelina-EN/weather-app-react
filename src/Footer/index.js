6abc6d66aed72df35fec2da0e622c0ab59577e86
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-03-16 16:31:29 +0100

    component Footer created

---
+++ src/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+  <footer class="footer">
+    <p>
+      <a href=" mailto: nieweglowska.ewelina@gmail.com">
+        <img src="/css/images/emailicon.png" width="15px" />
+        2022 Created by EN
+      </a>
+    </p>
+  </footer>
+);
+export default Footer;
